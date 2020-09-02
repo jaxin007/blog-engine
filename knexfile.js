7391ed269961979dea7878930d226f6ef474d400
etcetc1820 <etcetc1820@gmail.com>
2020-09-02 10:20:36 +0300

    be-5 setup database

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.PGHOST,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
