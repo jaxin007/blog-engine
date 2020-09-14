b5f73cce12a13ac1c6d9d40ad631dcdf9c8b17c1
jaxin007 <swatdimonbil@gmail.com>
2020-09-14 10:37:45 +0300

    dev fixed migrate script so we don't need run this script every time when building docker containers

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { EnvConfigInterface } from '../interfaces/EnvConfigInterface';
+import { EnvConfigInterface } from '../interfaces';
 
 export const config: EnvConfigInterface = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'dff$asdcAs',
 
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'secret',
 
-  PGHOST: process.env.PGHOST || 'localhost',
+  PGHOST: process.env.PGHOST || 'postgres',
   PGUSER: process.env.PGUSER || 'postgres',
   PGDATABASE: process.env.PGDATABASE || 'postgres',
   PGPASSWORD: process.env.PGPASSWORD || 'secret',
